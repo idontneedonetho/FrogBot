e85c4c7f8ad7b7ae1202345e75ba4dd299a57df3
idnot <thecrazyasylum@gmail.com>
2023-12-11 23:52:46 -0500

    added a new start point

--- commands/restart.py
+++ commands/restart.py
@@ -1,8 +1,8 @@
 # commands/restart.py
 
 from discord.ext import commands
-import subprocess
 import sys
+
 print('Restart.py loaded')
 
 def is_admin_or_user(user_id=126123710435295232):
@@ -15,20 +15,9 @@ def is_admin_or_user(user_id=126123710435295232):
 @commands.check(is_admin_or_user())
 async def restart_bot(ctx):
     print("Restarting bot...")
-
-    command = [sys.executable, "bot.py"]
-
-    try:
-        subprocess.Popen(command, close_fds=True)
-        await ctx.send("Bot restarting...")
-        await ctx.bot.close()
-        sys.exit()
-
-    except SystemExit:
-        pass
-
-    except Exception as e:
-        print(f"Error restarting the bot: {e}")
+    await ctx.send("Bot restarting...")
+    await ctx.bot.close()
+    sys.exit()
 
 def setup(bot):
     bot.add_command(restart_bot)
