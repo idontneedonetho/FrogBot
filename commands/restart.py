2007050ed7da4ea3f4d1133083478eefd48f2b52
idontneedonetho <thecrazyasylum@gmail.com>
2023-12-12 23:05:49 -0500

    Update restart.py

--- commands/restart.py
+++ commands/restart.py
@@ -3,6 +3,7 @@
 from discord.ext import commands
 import subprocess
 import sys
+import asyncio
 print('Restart.py loaded')
 
 RESTART_FLAG_FILE = 'restart.flag'
@@ -37,5 +38,23 @@ async def restart_bot(ctx):
     except Exception as e:
         print(f"Error restarting the bot: {e}")
 
+@commands.command(name="killbot")
+@is_admin_or_user()
+async def kill_bot(ctx):
+    confirmation_message = await ctx.send("Are you sure you want to shut down the bot? React with ✅ to confirm.")
+
+    await confirmation_message.add_reaction("✅")
+
+    def check(reaction, user):
+        return user == ctx.author and str(reaction.emoji) == '✅' and reaction.message.id == confirmation_message.id
+
+    try:
+        await ctx.bot.wait_for('reaction_add', timeout=60.0, check=check)
+        await ctx.send("Shutting down...")
+        await ctx.bot.close()
+    except asyncio.TimeoutError:
+        await ctx.send("Bot shutdown canceled.")
+            
 def setup(bot):
     bot.add_command(restart_bot)
+    bot.add_command(kill_bot)
