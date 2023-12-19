18271d67192bca1760b9b756ffc09c28aa0f29e2
idnot <thecrazyasylum@gmail.com>
2023-12-19 12:43:40 -0500

    Update emoji.py

--- commands/emoji.py
+++ commands/emoji.py
@@ -2,6 +2,7 @@
 
 import discord
 import sqlite3
+bot_replies = {}
 
 async def process_reaction(bot, payload, user_points):
     emoji_points = {
@@ -46,11 +47,23 @@ async def process_reaction(bot, payload, user_points):
     conn.close()
 
     print(f"Points added: {points_to_add} for {message.author.name} ({message.author.id})")
-    message_custom = emoji_messages.get(emoji_name, "")
+    new_reaction_message = f'{message.author.mention}{emoji_messages.get(emoji_name, "").format(points=points_to_add)}'
 
-    if message_custom:
-        message_custom_formatted = message_custom.format(points=points_to_add)
-        await channel.send(f'{message.author.mention}{message_custom_formatted}')
-    else:
-        points_formatted = "{:,}".format(user_points[author_id])
-        await channel.send(f'{message.author.mention} has been awarded {points_to_add} points! They now have {points_formatted}.')
+    # Check if the bot has already replied to this message
+    bot_reply_info = bot_replies.get(message.id)
+    if bot_reply_info:
+        bot_reply_id = bot_reply_info['reply_id']
+        try:
+            bot_reply_message = await channel.fetch_message(bot_reply_id)
+            # Append the new reaction message to the existing content
+            new_reply_content = bot_reply_info['content'] + "\nAnd " + new_reaction_message
+            await bot_reply_message.edit(content=new_reply_content)
+            bot_replies[message.id]['content'] = new_reply_content  # Update stored content
+        except discord.NotFound:
+            # If the reply message was not found, reset the entry
+            bot_replies[message.id] = None
+
+    if not bot_replies.get(message.id):
+        # Send a new reply and store the reply message ID and content
+        bot_reply_message = await message.reply(new_reaction_message)
+        bot_replies[message.id] = {'reply_id': bot_reply_message.id, 'content': new_reaction_message}
