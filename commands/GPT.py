777ae0ac3a65d5cd862cb2d7a653bd4251c76a8e
idnot <thecrazyasylum@gmail.com>
2023-12-23 02:04:13 -0500

    Added GPT 3.5 Turbo as a fall back for Gemini

--- commands/GPT.py
+++ commands/GPT.py
@@ -1,28 +1,45 @@
 # commands/GPT.py
 
 import google.generativeai as genai
+import openai
 import asyncio
 import os
 from dotenv import load_dotenv
 
-# Set your API key from an environment variable or a secure location
+# Set your API keys from environment variables or a secure location
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 # Initialize the Google AI client with your API key
 genai.configure(api_key=GOOGLE_API_KEY)
+# Initialize OpenAI
+openai.api_key = OPENAI_API_KEY
 
 async def ask_gpt(input_messages, retry_attempts=3, delay=1):
     for attempt in range(retry_attempts):
         try:
-            # Assuming you can start a chat and send messages in a similar way to OpenAI
             combined_messages = " ".join(msg['content'] for msg in input_messages if msg['role'] == 'user')
-            model = genai.GenerativeModel("gemini-pro")
+            model = genai.GenerativeModel(
+                model_name="gemini-pro",
+                safety_settings=None  # Disabling safety settings
+            )
             chat = model.start_chat()
             response = chat.send_message(combined_messages)
             return response.text
         except Exception as e:
-            print(f"Error in ask_gpt (attempt {attempt + 1}): {e}")
+            print(f"Error in ask_gpt with Google AI (attempt {attempt + 1}): {e}")
             if attempt < retry_attempts - 1:
                 await asyncio.sleep(delay)
+            else:
+                # Fallback to OpenAI if Google AI fails
+                try:
+                    response = openai.ChatCompletion.create(
+                        model="gpt-3.5-turbo",
+                        messages=[{"role": "user", "content": combined_messages}]
+                    )
+                    return response.choices[0].message['content']
+                except Exception as e:
+                    print(f"Error in ask_gpt with OpenAI: {e}")
+                    return "I'm sorry, I couldn't process that due to an error in both services."
 
-    return "I'm sorry, I couldn't process that due to a server error."
+    return "I'm sorry, I couldn't process that due to a server error with Google AI."
