e90dc955192effded0d836e0e46df87bb743d193
idontneedonetho <thecrazyasylum@gmail.com>
2023-12-24 23:05:17 -0500

    Update GPT.py

--- commands/GPT.py
+++ commands/GPT.py
@@ -3,10 +3,10 @@
 import google.generativeai as genai
 import openai
 from PIL import Image
-import re
+from pathlib import Path
 import asyncio
 import aiohttp
-import tempfile
+import uuid
 import os
 from dotenv import load_dotenv
 
@@ -26,13 +26,17 @@ safety_settings = {
 
 async def download_image(image_url):
     print(f"Downloading image from URL: {image_url}")
+    uid = str(uuid.uuid4())
+    images_dir = Path('./images')
+    images_dir.mkdir(exist_ok=True)
+    file_path = images_dir / f"{uid}.jpg"
+
     async with aiohttp.ClientSession() as session:
         async with session.get(image_url) as response:
             if response.status == 200:
-                with tempfile.NamedTemporaryFile(delete=False, suffix='.jpg') as temp_file:
-                    temp_file.write(await response.read())
-                    print(f"Image downloaded and saved to temporary file: {temp_file.name}")
-                    return temp_file.name
+                file_path.write_bytes(await response.read())
+                print(f"Image downloaded and saved as: {file_path}")
+                return uid
             else:
                 print(f"Failed to download image. HTTP status: {response.status}")
                 return None
@@ -45,42 +49,47 @@ async def process_image_with_google_api(temp_file_path):
     return response.text
 
 async def ask_gpt(input_messages, is_image=False, retry_attempts=3, delay=1):
-    try:
-        if is_image:
-            image_url = input_messages[0]['content']
-            if not re.search(r'\.(jpeg|jpg|png)', image_url.lower()):
-                print("Invalid image format detected.")
-                return "Invalid image format. Only JPEG and PNG are supported."
+    for attempt in range(retry_attempts):
+        try:
+            if is_image:
+                uid_found = False
+                for msg in input_messages:
+                    if msg['content'].startswith("Image UID:"):
+                        uid = msg['content'].split("Image UID:")[1].strip()
+                        uid_found = True
+                        break
 
-            temp_file_path = await download_image(image_url)
-            if temp_file_path is None:
-                return "Failed to download the image."
+                if uid_found:
+                    image_path = Path('./images') / f'{uid}.jpg'
+                    if not image_path.exists():
+                        print(f"Image not found at path: {image_path}")
+                        return "Image not found."
+                    response_text = await process_image_with_google_api(image_path)
+                    print("Image processing completed.")
+                    return response_text
+                else:
+                    print("No valid UID found in the message.")
+                    return "No valid UID found."
+            else:
+                combined_messages = " ".join(msg['content'] for msg in input_messages if msg['role'] == 'user')
+                model = genai.GenerativeModel(model_name="gemini-pro", safety_settings=safety_settings)
+                chat = model.start_chat()
+                print(f"Sending text to Google AI: {combined_messages}")
+                response = chat.send_message(combined_messages)
+                return response.text
 
-            response_text = await process_image_with_google_api(temp_file_path)
-            os.remove(temp_file_path)
-            print("Temporary file deleted after processing.")
-            return response_text
-        else:
-            combined_messages = " ".join(msg['content'] for msg in input_messages if msg['role'] == 'user')
-            model = genai.GenerativeModel(model_name="gemini-pro", safety_settings=safety_settings)
-            chat = model.start_chat()
-            print(f"Sending text to Google AI: {combined_messages}")
-            response = chat.send_message(combined_messages)
-            return response.text
-
-    except Exception as e:
-        print(f"Error in ask_gpt with Google AI: {e}")
-        if retry_attempts > 0:
-            await asyncio.sleep(delay)
-            return await ask_gpt(input_messages, is_image, retry_attempts - 1, delay)
-        else:
-            try:
-                print("Fallback to OpenAI due to Google AI failure.")
-                response = openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo",
-                    messages=[{"role": "user", "content": combined_messages}]
-                )
-                return response.choices[0].message['content']
-            except Exception as e:
-                print(f"Error in ask_gpt with OpenAI: {e}")
-                return "I'm sorry, I couldn't process that due to an error in both services."
+        except Exception as e:
+            print(f"Error in ask_gpt with Google AI: {e}")
+            if attempt < retry_attempts - 1:
+                await asyncio.sleep(delay)
+            else:
+                try:
+                    print("Fallback to OpenAI due to Google AI failure.")
+                    response = openai.ChatCompletion.create(
+                        model="gpt-3.5-turbo",
+                        messages=[{"role": "user", "content": combined_messages}]
+                    )
+                    return response.choices[0].message['content']
+                except Exception as e:
+                    print(f"Error in ask_gpt with OpenAI: {e}")
+    return "I'm sorry, I couldn't process that due to an error in both services."
