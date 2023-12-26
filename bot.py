519625eebcc0b51438687a810196a45e03ff6e51
idontneedonetho <thecrazyasylum@gmail.com>
2023-12-25 19:14:22 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -178,48 +178,49 @@ async def on_message(message):
     elif any(keyword in content_lower for keyword in ['primary mod']):
         await message.channel.send(':eyes:')
     elif bot.user.mentioned_in(message):
-        content = message.content.replace(bot.user.mention, '').strip()
-        if content:
-            ctx = await bot.get_context(message)
-            if ctx.valid:
-                await bot.process_commands(message)
-            else:
-                current_time = datetime.now()
-                last_request_time = user_request_times.get(message.author.id)
-                if last_request_time and current_time - last_request_time < RATE_LIMIT:
-                    try:
-                        await message.reply("You are sending messages too quickly. Please wait a moment before trying again.")
-                    except discord.HTTPException:
-                        await message.channel.send("Could not send the rate limit message; the original message might have been deleted.")
-                    return
-                user_request_times[message.author.id] = current_time
+    content = message.content.replace(bot.user.mention, '').strip()
+    if content:
+        ctx = await bot.get_context(message)
+        if ctx.valid:
+            await bot.process_commands(message)
+        else:
+            current_time = datetime.now()
+            last_request_time = user_request_times.get(message.author.id)
+            if last_request_time and current_time - last_request_time < RATE_LIMIT:
+                try:
+                    await message.reply("You are sending messages too quickly. Please wait a moment before trying again.")
+                except discord.HTTPException:
+                    await message.channel.send("Could not send the rate limit message; the original message might have been deleted.")
+                return
+            user_request_times[message.author.id] = current_time
 
-                async with message.channel.typing():
-                    context, uids = await fetch_reply_chain(message, max_tokens=4096)
-            
-                    is_image = bool(message.attachments or re.search(r'https?://\S+\.(jpg|jpeg|png)', message.content))
-                    if is_image:
-                        image_url = message.attachments[0].url if message.attachments else re.search(r'https?://\S+\.(jpg|jpeg|png)', message.content).group()
-                        uid = await GPT.download_image(image_url)
-                        if uid:
-                            content_for_gpt = content
-                            combined_messages = [{"role": "user", "content": msg} for msg in context] + [{"role": "user", "content": content_for_gpt}]
-                            async with gpt_semaphore:
-                                response = await GPT.ask_gpt(combined_messages, is_image=True)
-                                response = response.replace(bot.user.name + ":", "").strip()
-            
-                                response_with_uid = response + f"\n> Image UID: {uid}"
-                                await send_long_message(message, response_with_uid)
-                        else:
-                            print("Failed to download or save the image.")
-                    else:
+            async with message.channel.typing():
+                context, uids = await fetch_reply_chain(message, max_tokens=4096)
+
+                is_image = bool(message.attachments or re.search(r'https?://\S+\.(jpg|jpeg|png)', message.content))
+                if is_image:
+                    image_url = message.attachments[0].url if message.attachments else re.search(r'https?://\S+\.(jpg|jpeg|png)', message.content).group()
+                    uid = await GPT.download_image(image_url)
+                    if uid:
                         content_for_gpt = content
                         combined_messages = [{"role": "user", "content": msg} for msg in context] + [{"role": "user", "content": content_for_gpt}]
                         async with gpt_semaphore:
-                            response = await GPT.ask_gpt(combined_messages, is_image=False)
+                            response = await GPT.ask_gpt(combined_messages, is_image=True)
                             response = response.replace(bot.user.name + ":", "").strip()
-                            await send_long_message(message, response)
-            return
+
+                            # Append the UID to the response
+                            response_with_uid = response + f"\n> Image UID: {uid}"
+                            await send_long_message(message, response_with_uid)
+                    else:
+                        print("Failed to download or save the image.")
+                else:
+                    content_for_gpt = content
+                    combined_messages = [{"role": "user", "content": msg} for msg in context] + [{"role": "user", "content": content_for_gpt}]
+                    async with gpt_semaphore:
+                        response = await GPT.ask_gpt(combined_messages, is_image=False)
+                        response = response.replace(bot.user.name + ":", "").strip()
+                        await send_long_message(message, response)
+        return
     else:
         await bot.process_commands(message)
     
