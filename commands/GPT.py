a59d06753c1ac6af802e92cb0ac1a2e80bae5695
idontneedonetho <thecrazyasylum@gmail.com>
2024-01-19 23:58:43 -0500

    Update GPT.py

--- commands/GPT.py
+++ commands/GPT.py
@@ -22,7 +22,8 @@ def rate_limited_request():
     last_request_time = time.time()
 
 async def ask_gpt(input_messages, retry_attempts=3, delay=1):
-    frogbot_context = "You are FrogBot, a Discord bot assistant for all questions related to FrogPilot and OpenPilot. You are located in the FrogPilot discord and you answer to @FrogsGoMoo and @idnot . You'll keep your responses under 2000 characters. You will also provide snarky responses on occasion. Don't be kinky you weirdo."
+    gemini_context = "I am FrogBot, your assistant for all questions related to FrogPilot and OpenPilot. I'll keep my responses under 2000 characters."
+    assistant_id = "asst_koj1FbAIY2Y2eKxscCv2QcRV"
 
     for attempt in range(retry_attempts):
         rate_limited_request()
@@ -82,8 +83,15 @@ async def ask_gpt(input_messages, retry_attempts=3, delay=1):
             try:
                 model = GenerativeModel(model_name="gemini-pro")
                 chat = model.start_chat()
-                gemini_input_messages = [frogbot_context] + input_messages
-                response = chat.send_message("\n".join(gemini_input_messages))
+                gemini_input_messages = [gemini_context]
+                for msg in input_messages:
+                    if isinstance(msg, dict) and 'content' in msg:
+                        gemini_input_messages.append(msg['content'])
+                    elif isinstance(msg, str):
+                        gemini_input_messages.append(msg)
+                gemini_input_messages = [str(msg) for msg in gemini_input_messages]
+                gemini_input_message_str = "\n".join(gemini_input_messages)
+                response = chat.send_message(gemini_input_message_str)
                 return response.text
             except Exception as e:
                 print(f"Error in ask_gpt with Google AI: {e}")
