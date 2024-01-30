88e94cd4f879d6b218de57301751aa70ff9c701d
Ethan Smith <46655952+Joeslinky@users.noreply.github.com>
2024-01-29 19:55:33 -0600

    Update restart.py

--- modules/restart.py
+++ modules/restart.py
@@ -16,6 +16,8 @@ core_script = root_dir / 'core.py'
 @is_admin_or_user()
 async def restart_bot(ctx):
     await ctx.send("Restarting bot, please wait...")
+    if os.path.exists("restart_channel_id.txt"):
+        os.remove("restart_channel_id.txt")
     with open("restart_channel_id.txt", "w") as file:
         file.write(str(ctx.channel.id))
     for cmd in list(ctx.bot.all_commands.keys()):
