c6154e455b107d4251e2898c29df3f8d7de87cec
idnot <thecrazyasylum@gmail.com>
2024-01-31 12:56:35 -0500

    Update welcome.py

--- modules/welcome.py
+++ modules/welcome.py
@@ -4,7 +4,7 @@ import random
 async def on_member_join(member):
     welcome_channel = member.guild.system_channel
     if welcome_channel:
-        await asyncio.sleep(1)
+        await asyncio.sleep(5)
         gif_links = ["https://cdn3.emoji.gg/emojis/1463-wave.gif"] * 49 + \
                     ["https://i.pinimg.com/originals/ab/bd/b6/abbdb6e66ec39dc9262abc617fbc2b02.gif"] * 49 + \
                     ["https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExYzhsN3Fnd2c1MG1hcmhwMG00czE5ZHZoZmZsa3k4N3hqcWJya2NwdiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/5xtDarIELDLO7lSFQJi/giphy.gif"]
