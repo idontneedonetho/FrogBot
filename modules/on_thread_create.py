f27d8ce6084e10499b499ce3afa55304439c399c
idnot <thecrazyasylum@gmail.com>
2024-02-03 15:32:59 -0500

    Update on_thread_create.py

--- modules/on_thread_create.py
+++ modules/on_thread_create.py
@@ -8,7 +8,8 @@ async def on_thread_create(thread):
             emojis_to_add = ["🐞", "📜", "📹"]
         if thread.parent_id in [1167651506560962581, 1160318669839147259]:
             emojis_to_add = ["💡", "🧠"]
-        first_message = await thread.fetch_message(thread.id)
+        thread = await thread.fetch_thread()
+        first_message = thread.messages[0]
         for emoji in emojis_to_add:
             await first_message.add_reaction(emoji)
     except Exception as e:
