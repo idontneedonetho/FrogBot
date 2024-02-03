503ec3aaa5c6775c5b5222fa5d6d43ea7cc8106a
idnot <thecrazyasylum@gmail.com>
2024-02-03 17:35:48 -0500

    Update update.py

--- modules/update.py
+++ modules/update.py
@@ -2,7 +2,6 @@
 
 from discord.ext import commands
 from modules.utils.commons import is_admin_or_user
-import subprocess
 import asyncio
 
 @commands.command(name="update")
@@ -10,23 +9,20 @@ import asyncio
 async def git_pull(ctx, branch="beta"):
     print('update command invoked')
     try:
-        current_branch = (await asyncio.create_subprocess_exec(
+        current_branch_proc = await asyncio.create_subprocess_exec(
             "git", "rev-parse", "--abbrev-ref", "HEAD",
-            stdout=subprocess.PIPE)).stdout.decode().strip()
-        
+            stdout=asyncio.subprocess.PIPE)
+        stdout, _ = await current_branch_proc.communicate()
+        current_branch = stdout.decode().strip()
         if current_branch != branch:
             await ctx.send(f"Switching to branch {branch}")
             await asyncio.create_subprocess_exec("git", "checkout", branch)
-        
         await git_stash(ctx)
-        
-        process = await asyncio.create_subprocess_exec(
+        pull_proc = await asyncio.create_subprocess_exec(
             'git', 'pull', 'origin', branch,
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        
-        stdout, stderr = await process.communicate()
-        
-        if process.returncode == 0:
+            stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+        stdout, stderr = await pull_proc.communicate()
+        if pull_proc.returncode == 0:
             print('Git pull successful.')
             await ctx.send('Git pull successful.')
         else:
@@ -36,11 +32,11 @@ async def git_pull(ctx, branch="beta"):
         print(f'Error updating the script: {e}')
         await ctx.send(f'Error updating the script: {e}')
 
-
 async def git_stash(ctx):
     try:
         print("Stashing changes...")
-        subprocess.run(["git", "stash"])
+        stash_proc = await asyncio.create_subprocess_exec("git", "stash")
+        await stash_proc.communicate()
         print("Changes stashed successfully.")
         await ctx.send('Changes stashed successfully.')
     except Exception as e:
