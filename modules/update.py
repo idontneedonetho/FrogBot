83cbd6897ec17ef4e1a05b1d1640ae08a1b0082c
idnot <thecrazyasylum@gmail.com>
2024-02-03 17:32:36 -0500

    Update update.py

--- modules/update.py
+++ modules/update.py
@@ -3,28 +3,39 @@
 from discord.ext import commands
 from modules.utils.commons import is_admin_or_user
 import subprocess
+import asyncio
 
 @commands.command(name="update")
 @is_admin_or_user()
 async def git_pull(ctx, branch="beta"):
     print('update command invoked')
     try:
-        current_branch = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).decode().strip()
+        current_branch = (await asyncio.create_subprocess_exec(
+            "git", "rev-parse", "--abbrev-ref", "HEAD",
+            stdout=subprocess.PIPE)).stdout.decode().strip()
+        
         if current_branch != branch:
             await ctx.send(f"Switching to branch {branch}")
-            subprocess.run(["git", "checkout", branch])
+            await asyncio.create_subprocess_exec("git", "checkout", branch)
+        
         await git_stash(ctx)
-        process = subprocess.Popen(['git', 'pull', 'origin', branch], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output, error = process.communicate()
+        
+        process = await asyncio.create_subprocess_exec(
+            'git', 'pull', 'origin', branch,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
+        stdout, stderr = await process.communicate()
+        
         if process.returncode == 0:
             print('Git pull successful.')
             await ctx.send('Git pull successful.')
         else:
-            print(f'Error updating the script: {error.decode()}')
-            await ctx.send(f'Error updating the script: {error.decode()}')
+            print(f'Error updating the script: {stderr.decode()}')
+            await ctx.send(f'Error updating the script: {stderr.decode()}')
     except Exception as e:
         print(f'Error updating the script: {e}')
         await ctx.send(f'Error updating the script: {e}')
+
 
 async def git_stash(ctx):
     try:
