f5deebb4ffdde3611218e18cdc8daace04adba7d
idnot <thecrazyasylum@gmail.com>
2024-02-11 13:23:35 -0500

    Update GPT.py

--- modules/utils/GPT.py
+++ modules/utils/GPT.py
@@ -7,7 +7,7 @@ from llama_index.vector_stores import QdrantVectorStore
 from llama_index.embeddings import OpenAIEmbedding
 from llama_index.memory import ChatMemoryBuffer
 from qdrant_client import QdrantClient
-from llama_index.llms import Gemini
+from llama_index.llms import OpenAI
 from dotenv import load_dotenv
 import openai
 import os
@@ -16,18 +16,11 @@ import re
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-safety_settings = {
-    "HARM_CATEGORY_SEXUALLY_EXPLICIT": "BLOCK_NONE",
-    "HARM_CATEGORY_HATE_SPEECH": "BLOCK_NONE",
-    "HARM_CATEGORY_HARASSMENT": "BLOCK_NONE",
-    "HARM_CATEGORY_DANGEROUS_CONTENT": "BLOCK_NONE"
-}
-
 try:
     client = QdrantClient(os.getenv('QDRANT_URL'), api_key=os.getenv('QDRANT_API'))
-    vector_store = QdrantVectorStore(client=client, collection_name="openpilot-data")
+    vector_store = QdrantVectorStore(client=client, collection_name="openpilot-data-gemini")
     embed_model = OpenAIEmbedding(model="text-embedding-3-small")
-    llm = Gemini(model="gemini-pro", max_tokens=1000, safety_settings=safety_settings)
+    llm = OpenAI(model="gpt-4-turbo-preview", max_tokens=1000)
     storage_context = StorageContext.from_defaults(vector_store=vector_store)
     service_context = ServiceContext.from_defaults(embed_model=embed_model, llm=llm, chunk_overlap=24, chunk_size=1024)
     index = VectorStoreIndex.from_vector_store(vector_store, service_context=service_context)
