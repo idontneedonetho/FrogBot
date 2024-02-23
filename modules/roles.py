b7ddd7831c417fba594b23eb1fde230cb968f806
idnot <thecrazyasylum@gmail.com>
2024-02-23 16:11:27 -0500

    static roles
    make sure users keep the same roles between leaving and rejoining

--- modules/roles.py
+++ modules/roles.py
@@ -1,6 +1,6 @@
 # modules.roles
 
-from modules.utils.database import db_access_with_retry
+from modules.utils.database import db_access_with_retry, update_user_roles
 from modules.utils.progression import role_thresholds
 import disnake
 
@@ -29,6 +29,12 @@ async def manage_roles(member, appropriate_role, is_upgrade, notification_channe
         await member.remove_roles(*roles_to_remove, reason="Removing outdated roles based on points")
         if is_upgrade and roles_to_add and notification_channel:
             await notification_channel.send(f"Congratulations! {member.mention} has been granted the following role: {appropriate_role.name}!")
+    update_user_roles(member.id, member.roles)
+
+async def get_user_roles(client, user_id):
+    role_ids = db_access_with_retry('SELECT role_id FROM user_roles WHERE user_id = ?', (user_id,))
+    guild = await get_guild(client)
+    return [guild.get_role(role_id) for role_id in role_ids]
 
 async def check_user_points(client):
     user_points_data = db_access_with_retry('SELECT user_id, points FROM user_points')
@@ -40,6 +46,8 @@ async def check_user_points(client):
         member = guild.get_member(user_id)
         if member is None:
             print(f"Member with ID {user_id} not found in guild.")
+            member_roles = await get_user_roles(client, user_id)
+            member.roles = member_roles
             continue
         appropriate_role = next((guild.get_role(role_id) for threshold, role_id in sorted(role_thresholds.items(), reverse=True) if points >= threshold), None)
         highest_existing_role = max(member.roles, key=lambda r: r.position, default=None)
