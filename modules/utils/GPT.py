889092f3748189d66bcc4c90855ad25e192c077e
idnot <thecrazyasylum@gmail.com>
2024-03-08 20:57:54 -0500

    Update GPT.py

--- modules/utils/GPT.py
+++ modules/utils/GPT.py
@@ -2,8 +2,8 @@
 
 from modules.utils.commons import send_long_message, fetch_reply_chain, HistoryChatMessage
 from llama_index.core import StorageContext, Settings, VectorStoreIndex
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.vector_stores.qdrant import QdrantVectorStore
-from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.core.llms import MessageRole as Role
 from llama_index.core.memory import ChatMemoryBuffer
 from llama_index.llms.openai import OpenAI
@@ -15,12 +15,13 @@ import re
 import os
 
 load_dotenv()
+os.environ["GOOGLE_API_KEY"] = os.getenv('GOOGLE_API_KEY')
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 client = QdrantClient(os.getenv('QDRANT_URL'), api_key=os.getenv('QDRANT_API'))
-vector_store = QdrantVectorStore(client=client, collection_name="openpilot-data")
+vector_store = QdrantVectorStore(client=client, enable_hybrid=True, batch_size=20, collection_name="openpilot-data-sparse")
 Settings.llm = OpenAI(model="gpt-4-turbo-preview", max_tokens=1000)
-Settings.embed_model = OpenAIEmbedding(model="text-embedding-3-small")
+Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
 index = VectorStoreIndex.from_vector_store(vector_store)
 
@@ -34,14 +35,17 @@ async def process_message_with_llm(message, client):
                 memory.set(context + [HistoryChatMessage(f"{content}", Role.USER)])
                 chat_engine = index.as_chat_engine(
                     chat_mode="condense_plus_context",
-                    similarity_top_k=5,
+                    similarity_top_k=4,
+                    sparse_top_k=12,
+                    vector_store_query_mode="hybrid",
                     memory=memory,
                     system_prompt=(
                         f"You are {client.user.name}, a Discord chat bot. "
                         "The topic is OpenPilot and its various forks.\n"
                         "Always provide the links to the sources of the information, if applicable. "
                         "Always give an answer to the best of your abilities."
-                    )
+                    ),
+                    verbose=True,
                 )
                 chat_response = await asyncio.to_thread(chat_engine.chat, content)
                 if not chat_response or not chat_response.response:
