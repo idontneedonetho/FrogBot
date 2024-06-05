eed60e1f365ece6ad04c6bd30bfd6344b33d96ff
idnot <thecrazyasylum@gmail.com>
2024-06-05 10:17:52 -0400

    big change in requirements coming soon
    Moving to postgresql db

--- local index creation.py
+++ local index creation.py
@@ -3,10 +3,12 @@
 from llama_index.core import VectorStoreIndex, Settings, StorageContext, SimpleDirectoryReader
 from llama_index.readers.github import GithubClient, GithubRepositoryReader
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.vector_stores.duckdb import DuckDBVectorStore
+from llama_index.vector_stores.postgres import PGVectorStore
 from llama_index.readers.web import WholeSiteReader
+from sqlalchemy import make_url
 from dotenv import load_dotenv
 from tqdm import tqdm
+import psycopg2
 import httpx
 import torch
 import sys
@@ -17,135 +19,126 @@ github_client = GithubClient(os.getenv('GITHUB_TOKEN'))
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print("GPU available:", torch.cuda.is_available())
 Settings.embed_model = HuggingFaceEmbedding(model_name="avsolatorio/NoInstruct-small-Embedding-v0", device=device)
+
+'''POSTGRES DATABASE CREATION'''
+connection_string = os.getenv('POSTGRES_CONNECTION_STRING')
+db_name = "bot_vector_db"
+conn = psycopg2.connect(connection_string)
+conn.autocommit = True
+with conn.cursor() as c:
+    c.execute(f"SELECT datname FROM pg_catalog.pg_database WHERE lower(datname) = lower('{db_name}')")
+    exists = c.fetchone()
+    if not exists:
+        c.execute(f"CREATE DATABASE {db_name}")
+url = make_url(connection_string)
 embed_dim=384
 
 '''DISCORD DATA'''
 print("Loading local files...")
-dir_path = 'DiscordDocs'
-reader = SimpleDirectoryReader(input_dir=dir_path, required_exts=[".txt"])
+reader = SimpleDirectoryReader(input_dir="DiscordDocs")
 discord_docs = reader.load_data()
-
 print("Local files loaded successfully. Setting up vector store for Discord data...")
-discord_vector_store = DuckDBVectorStore(database_name="discord.duckdb", embed_dim=embed_dim, persist_dir="./db_files/")
+discord_vector_store = PGVectorStore.from_params(
+    database=db_name,
+    host=url.host,
+    password=url.password,
+    port=url.port,
+    user=url.username,
+    table_name="discord_docs",
+    embed_dim=embed_dim,
+    hybrid_search=True,
+    text_search_config="english",
+)
 discord_storage_context = StorageContext.from_defaults(vector_store=discord_vector_store)
-
 discord_index = VectorStoreIndex.from_documents(discord_docs, storage_context=discord_storage_context, show_progress=True)
 print("Discord data setup complete.")
 
-'''WIKI DATA'''
-scraper = WholeSiteReader(
-    prefix="https://github.com/commaai/openpilot/wiki/", max_depth=2
-)
-wiki = scraper.load_data(
-    base_url="https://github.com/commaai/openpilot/wiki"
-)
+# '''WIKI DATA'''
+# scraper = WholeSiteReader(
+#     prefix="https://github.com/commaai/openpilot/wiki/", max_depth=2
+# )
+# wiki = scraper.load_data(
+#     base_url="https://github.com/commaai/openpilot/wiki"
+# )
+# print("Wiki data downloaded successfully. Setting up vector store for wiki...")
+# wiki_vector_store = PGVectorStore.from_params(
+#     database=db_name,
+#     host=url.host,
+#     password=url.password,
+#     port=url.port,
+#     user=url.username,
+#     table_name="wiki_docs",
+#     embed_dim=embed_dim,
+#     hybrid_search=True,
+#     text_search_config="english",
+# )
+# wiki_storage_context = StorageContext.from_defaults(vector_store=wiki_vector_store)
+# wiki_index = VectorStoreIndex.from_documents(wiki, storage_context=wiki_storage_context, show_progress=True)
+# print("Wiki index setup complete.")
 
-print("Wiki data downloaded successfully. Setting up vector store for wiki...")
-wiki_vector_store = DuckDBVectorStore(database_name="wiki.duckdb", embed_dim=embed_dim, persist_dir="./db_files/")
-wiki_storage_context = StorageContext.from_defaults(vector_store=wiki_vector_store)
+# '''GITHUB DATA'''
+# repos_config = [
+#     {
+#         "owner": "twilsonco",
+#         "repo": "openpilot",
+#         "branch": "log-info",
+#         "filter_directories": (["sec"], GithubRepositoryReader.FilterType.INCLUDE),
+#         "filter_file_extensions": ([".md"], GithubRepositoryReader.FilterType.INCLUDE),
+#     },
+#     {
+#         "owner": "commaai",
+#         "repo": "openpilot-docs",
+#         "branch": "gh-pages",
+#         "filter_directories": (["docs"], GithubRepositoryReader.FilterType.INCLUDE),
+#         "filter_file_extensions": ([".s"], GithubRepositoryReader.FilterType.EXCLUDE),
+#     },
+#     {
+#         "owner": "commaai",
+#         "repo": "comma10k",
+#         "branch": "master",
+#         "filter_directories": (["imgs", "imgs2", "imgsd", "masks", "masks2", "masksd"], GithubRepositoryReader.FilterType.EXCLUDE),
+#         "filter_file_extensions": ([".png", ".jpg"], GithubRepositoryReader.FilterType.EXCLUDE),
+#     },
+#     {
+#         "owner": "FrogAi",
+#         "repo": "FrogPilot",
+#         "branch": "FrogPilot-Development",
+#         "filter_directories": (["selfdrive", "README.md", "docs", "tools"], GithubRepositoryReader.FilterType.INCLUDE),
+#         "filter_file_extensions": ([".py", ".md", ".h", ".cc"], GithubRepositoryReader.FilterType.INCLUDE),
+#     },
+# ]
 
-wiki_index = VectorStoreIndex.from_documents(wiki, storage_context=wiki_storage_context, show_progress=True)
-print("Wiki index setup complete.")
+# for config in tqdm(repos_config, desc="Loading documents from repositories"):
+#     try:
+#         loader = GithubRepositoryReader(
+#             github_client,
+#             owner=config["owner"],
+#             repo=config["repo"],
+#             filter_directories=config["filter_directories"],
+#             filter_file_extensions=config["filter_file_extensions"],
+#             verbose=False,
+#             concurrent_requests=10,
+#             timeout=10,
+#             retries=3,
+#         )
+#         documents = loader.load_data(branch=config["branch"])
 
-'''RETROPILOT STOREFRONT DATA'''
-retropilot_scraper = WholeSiteReader(
-    prefix="https://shop.retropilot.org/", max_depth=2
-)
-retropilot_data = retropilot_scraper.load_data(
-    base_url="https://shop.retropilot.org/"
-)
+#         print(f"{config['owner']}/{config['repo']} data downloaded successfully. Setting up vector store...")
+#         github_vector_store = PGVectorStore.from_params(
+#             database=db_name,
+#             host=url.host,
+#             password=url.password,
+#             port=url.port,
+#             user=url.username,
+#             table_name=f"{config['owner']}-{config['repo']}_docs",
+#             embed_dim=embed_dim,
+#             hybrid_search=True,
+#             text_search_config="english",
+#         )
+#         github_storage_context = StorageContext.from_defaults(vector_store=github_vector_store)
+#         github_index = VectorStoreIndex.from_documents(documents, storage_context=github_storage_context, show_progress=True)
+#         print(f"{config['owner']}/{config['repo']} index setup complete.")
 
-print("RetroPilot data downloaded successfully. Setting up vector store for RetroPilot...")
-retropilot_vector_store = DuckDBVectorStore(database_name="retropilot.duckdb", embed_dim=embed_dim, persist_dir="./db_files/")
-retropilot_storage_context = StorageContext.from_defaults(vector_store=retropilot_vector_store)
-
-retropilot_index = VectorStoreIndex.from_documents(retropilot_data, storage_context=retropilot_storage_context, show_progress=True)
-print("RetroPilot index setup complete.")
-
-'''ONECLONE DATA'''
-oneclone_scraper = WholeSiteReader(
-    prefix="https://oneclone.net/", max_depth=2
-)
-oneclone_data = oneclone_scraper.load_data(
-    base_url="https://oneclone.net/"
-)
-
-print("OneClone data downloaded successfully. Setting up vector store for OneClone...")
-oneclone_vector_store = DuckDBVectorStore(database_name="oneclone.duckdb", embed_dim=embed_dim, persist_dir="./db_files/")
-oneclone_storage_context = StorageContext.from_defaults(vector_store=oneclone_vector_store)
-
-oneclone_index = VectorStoreIndex.from_documents(oneclone_data, storage_context=oneclone_storage_context, show_progress=True)
-print("OneClone index setup complete.")
-
-'''SPRINGER ELECTRONICS DATA'''
-springer_scraper = WholeSiteReader(
-    prefix="https://springerelectronics.com/", max_depth=2
-)
-springer_data = springer_scraper.load_data(
-    base_url="https://springerelectronics.com/"
-)
-
-print("Springer Electronics data downloaded successfully. Setting up vector store for Springer Electronics...")
-springer_vector_store = DuckDBVectorStore(database_name="springer.duckdb", embed_dim=embed_dim, persist_dir="./db_files/")
-springer_storage_context = StorageContext.from_defaults(vector_store=springer_vector_store)
-
-springer_index = VectorStoreIndex.from_documents(springer_data, storage_context=springer_storage_context, show_progress=True)
-print("Springer Electronics index setup complete.")
-
-'''GITHUB DATA'''
-repos_config = [
-    {
-        "owner": "twilsonco",
-        "repo": "openpilot",
-        "branch": "log-info",
-        "filter_directories": (["sec"], GithubRepositoryReader.FilterType.INCLUDE),
-        "filter_file_extensions": ([".md"], GithubRepositoryReader.FilterType.INCLUDE),
-    },
-    {
-        "owner": "commaai",
-        "repo": "openpilot-docs",
-        "branch": "gh-pages",
-        "filter_directories": (["docs"], GithubRepositoryReader.FilterType.INCLUDE),
-        "filter_file_extensions": ([".s"], GithubRepositoryReader.FilterType.EXCLUDE),
-    },
-    {
-        "owner": "commaai",
-        "repo": "comma10k",
-        "branch": "master",
-        "filter_directories": (["imgs", "imgs2", "imgsd", "masks", "masks2", "masksd"], GithubRepositoryReader.FilterType.EXCLUDE),
-        "filter_file_extensions": ([".png", ".jpg"], GithubRepositoryReader.FilterType.EXCLUDE),
-    },
-    {
-        "owner": "FrogAi",
-        "repo": "FrogPilot",
-        "branch": "FrogPilot-Development",
-        "filter_directories": (["selfdrive", "README.md", "docs", "tools"], GithubRepositoryReader.FilterType.INCLUDE),
-        "filter_file_extensions": ([".py", ".md", ".h", ".cc"], GithubRepositoryReader.FilterType.INCLUDE),
-    },
-]
-
-for config in tqdm(repos_config, desc="Loading documents from repositories"):
-    try:
-        loader = GithubRepositoryReader(
-            github_client,
-            owner=config["owner"],
-            repo=config["repo"],
-            filter_directories=config["filter_directories"],
-            filter_file_extensions=config["filter_file_extensions"],
-            verbose=False,
-            concurrent_requests=10,
-            timeout=10,
-            retries=3,
-        )
-        documents = loader.load_data(branch=config["branch"])
-
-        print(f"{config['owner']}/{config['repo']} data downloaded successfully. Setting up vector store...")
-        github_vector_store = DuckDBVectorStore(database_name=f"{config['owner']}-{config['repo']}.duckdb", embed_dim=embed_dim, persist_dir="./db_files/")
-        github_storage_context = StorageContext.from_defaults(vector_store=github_vector_store)
-
-        github_index = VectorStoreIndex.from_documents(documents, storage_context=github_storage_context, show_progress=True)
-        print(f"{config['owner']}/{config['repo']} index setup complete.")
-
-    except httpx.ConnectTimeout:
-        print(f"Connection timeout for {config['owner']}/{config['repo']}.", file=sys.stderr)
-        sys.exit(1)
+#     except httpx.ConnectTimeout:
+#         print(f"Connection timeout for {config['owner']}/{config['repo']}.", file=sys.stderr)
+#         sys.exit(1)
--- modules/utils/GPT.py
+++ modules/utils/GPT.py
@@ -2,29 +2,39 @@
 
 from llama_index.core.tools import QueryEngineTool, ToolMetadata, FunctionTool
 from modules.utils.commons import send_long_message, fetch_reply_chain
+from llama_index.core.response_synthesizers import CompactAndRefine
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.tools.duckduckgo import DuckDuckGoSearchToolSpec
-from llama_index.vector_stores.duckdb import DuckDBVectorStore
+from llama_index.core.query_engine import RetrieverQueryEngine
+from llama_index.core.retrievers import QueryFusionRetriever
+from llama_index.vector_stores.postgres import PGVectorStore
 from llama_index.core import Settings, VectorStoreIndex
-from llama_index.core.memory import ChatMemoryBuffer
 from llama_index.agent.openai import OpenAIAgent
 from llama_index.core.agent import ReActAgent
 from llama_index.core.llms import ChatMessage
 from llama_index.llms.openai import OpenAI
+from sqlalchemy import make_url
 from dotenv import load_dotenv
 import asyncio
 import openai
 import torch
-import re
 import os
 
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
-Settings.llm = OpenAI(model="gpt-4o", max_tokens=1000)
+Settings.llm = OpenAI(model="gpt-3.5-turbo", max_tokens=1000)
+# Settings.llm = OpenAI(model="gpt-4o", max_tokens=1000)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print("GPU available:", torch.cuda.is_available())
 Settings.embed_model = HuggingFaceEmbedding(model_name="avsolatorio/NoInstruct-small-Embedding-v0", device=device)
 
+'''POSTGRES DATABASE CREATION'''
+connection_string = os.getenv('POSTGRES_CONNECTION_STRING')
+db_name = "bot_vector_db"
+url = make_url(connection_string)
+embed_dim=384
+
+'''GOOGLE SEARCH TOOL'''
 google_search_spec = DuckDuckGoSearchToolSpec()
 search_spec = DuckDuckGoSearchToolSpec()
 def site_search(input, site):
@@ -41,12 +51,41 @@ for site in sites:
         tool_metadata=ToolMetadata(name=f"{t_name}_Search_Tool", description=desc)
     )
 
+'''QUERY ENGINE TOOLS'''
 def create_query_engine(collection_name, tool_name, description):
-    vector_store = DuckDBVectorStore(database_name=f"{collection_name}.duckdb", embed_dim=384, persist_dir="./db_files/")
+    vector_store = PGVectorStore.from_params(
+        database=db_name,
+        host=url.host,
+        password=url.password,
+        port=url.port,
+        user=url.username,
+        table_name=f"{collection_name}_docs",
+        embed_dim=embed_dim,
+        hybrid_search=True,
+        text_search_config="english",
+        cache_ok=True,
+    )
     index = VectorStoreIndex.from_vector_store(vector_store)
-    engine = index.as_query_engine()
+    vector_retriever = index.as_retriever(
+        vector_store_query_mode="default",
+        similarity_top_k=5,
+    )
+    text_retriever = index.as_retriever(
+        vector_store_query_mode="sparse",
+        similarity_top_k=12,
+    )
+    retriever = QueryFusionRetriever(
+        [vector_retriever, text_retriever],
+        similarity_top_k=5,
+        num_queries=1,
+        mode="relative_score",
+    )
+    response_synthesizer = CompactAndRefine()
     tool = QueryEngineTool(
-        query_engine=engine,
+        query_engine=RetrieverQueryEngine(
+            retriever=retriever,
+            response_synthesizer=response_synthesizer,
+        ),
         metadata=ToolMetadata(
             name=tool_name,
             description=description,
@@ -55,12 +94,12 @@ def create_query_engine(collection_name, tool_name, description):
     return tool
 
 collections = {
-    "discord": {"tool_name": "Discord_Tool", "description": "Discord data containing information from the OpenPilot community."},
-    "FrogAi-FrogPilot": {"tool_name": "FrogPilot_Tool", "description": "The code base and settings for FrogPilot/OpenPilot."},
-    "twilsonco-openpilot": {"tool_name": "NNFF_Tool", "description": "The write up and breakdown of NNFF(Neural Network FeedForward) and how it works for OpenPilot. https://github.com/twilsonco/openpilot/tree/log-info"},
-    "commaai-openpilot-docs": {"tool_name": "OpenPilot_Docs_Tool", "description": "The official OpenPilot documentation. This DOES NOT include any code or code-related information like settings."},
-    "commaai-comma10k": {"tool_name": "Comma10k_Tool", "description": "The comma10k, also known as 'comma pencil', dataset and information."},
-    "wiki": {"tool_name": "Wiki_Tool", "description": "The OpenPilot wiki data, contains some sparse but detailed information."},
+    "discord": {"tool_name": "Discord_Tool", "description": "Data from Discord related to the OpenPilot community."},
+    "FrogAi-FrogPilot": {"tool_name": "FrogPilot_Tool", "description": "This includes configuration settings and the source code for FrogPilot/OpenPilot."},
+    "twilsonco-openpilot": {"tool_name": "NNFF_Tool", "description": "This provides a detailed explanation and analysis of the Neural Network FeedForward (NNFF) as used in OpenPilot. More details can be found at: https://github.com/twilsonco/openpilot/tree/log-info"},
+    "commaai-openpilot-docs": {"tool_name": "OpenPilot_Docs_Tool", "description": "This is the official OpenPilot documentation. Note that it does not contain any code or information related to code settings."},
+    "commaai-comma10k": {"tool_name": "Comma10k_Tool", "description": "This includes the comma10k dataset, also known as 'comma pencil', and its related information."},
+    "wiki": {"tool_name": "Wiki_Tool", "description": "This contains data from the OpenPilot wiki, which includes some detailed, though not extensive, information."},
 }
 
 query_engine_tools = [create_query_engine(name, data["tool_name"], data["description"]) for name, data in collections.items()]
@@ -81,11 +120,10 @@ async def process_message_with_llm(message, client):
                     'default': (
                         "Provide accurate responses using server and channel names. "
                         "Give context and related information. Use available tools. "
-                        "Maintain respectfulness. Search unknown acronyms with Discord_Tool. "
+                        "Maintain respectfulness. If an unknown acronym is encountered, use Discord_Tool to find its meaning and then perform another search with the newly found information. "
                         "Provide source links. Use multiple tools for comprehensive responses."
                     )
                 }
-
                 system_prompt = (
                     f"Assistant: '{client.user}'. "
                     f"Channel: '{message.channel}'. "
@@ -94,7 +132,6 @@ async def process_message_with_llm(message, client):
                     "Provide accurate information and support as an OpenPilot community assistant. "
                     "Respond appropriately to the conversation context. "
                     "Avoid code interaction instructions unless asked. Examine code for answers. "
-                    "Remind users of alpha status and to reply directly to your messages."
                 )
                 bug_reports_forum_channel_id = 1162100167110053888
                 if hasattr(message.channel, 'parent_id'):
@@ -105,21 +142,32 @@ async def process_message_with_llm(message, client):
                         system_prompt += channel_prompts['default']
                 else:
                     system_prompt += channel_prompts['default']
-                chat_engine = OpenAIAgent.from_tools(
-                    query_engine_tools,
-                    system_prompt=system_prompt,
-                    verbose=True,
-                    max_iterations=20,
-                    chat_history=chat_history,
-                )
+                try:
+                    chat_engine = OpenAIAgent.from_tools(
+                        query_engine_tools,
+                        system_prompt=system_prompt,
+                        verbose=True,
+                        chat_history=chat_history,
+                    )
+                except Exception as e:
+                    print(f"OpenAIAgent failed with error: {str(e)}. Switching to ReActAgent.")
+                    chat_engine = ReActAgent.from_tools(
+                        query_engine_tools,
+                        system_prompt=system_prompt,
+                        verbose=True,
+                        max_iterations=20,
+                        chat_history=chat_history,
+                    )
                 chat_history.append(ChatMessage(content=content, role="user"))
                 chat_response = await asyncio.to_thread(chat_engine.chat, content)
                 if not chat_response or not chat_response.response:
                     await message.channel.send("There was an error processing the message." if not chat_response else "I didn't get a response.")
                     return
                 chat_history.append(ChatMessage(content=chat_response.response, role="assistant"))
-                response_text = chat_response.response
-                response_text = re.sub(r'^[^:]+:\s(?=[A-Z])', '', response_text)
+                response_text = [chat_response.response]
+                if not reply_chain:
+                    response_text.append(f"*Note: Reply directly to {client.user.mention}'s messages to maintain conversation context.*")
+                response_text = '\n'.join(response_text)
                 await send_long_message(message, response_text)
         except Exception as e:
             await message.channel.send(f"An error occurred: {str(e)}")
