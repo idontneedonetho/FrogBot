77b69f421231105f42a364d857e382ce61095f46
idontneedonetho <thecrazyasylum@gmail.com>
2025-05-17 17:31:33 -0400

    more

--- core.py
+++ core.py
@@ -13,7 +13,7 @@ import yaml
 import sys
 
 CONFIG = {
-    'VERSION': 'v3.7',
+    'VERSION': 'v3.7.1',
     'CONFIG_FILE': Path('config.yaml'),
     'COGS_DIR': Path("modules"),
     'TEST_GUILDS': [698205243103641711, 1137853399715549214],
